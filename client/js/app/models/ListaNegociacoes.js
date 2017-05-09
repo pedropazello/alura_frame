bb17a94fedbb98eb63a50a620a9c34c2cfbc5d2e
Pedro Pazello <email@pedropazello.com>
2017-05-08 22:24:01 -0300

    Implementa a primeira aula da parte II do curso

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,25 @@
 class ListaNegociacoes {
-    
-    constructor() {
-        
+
+    constructor(callback) {
+
         this._negociacoes = [];
+        this._callback = callback;
     }
-    
+
     adiciona(negociacao) {
-        
+
         this._negociacoes.push(negociacao);
+        this._callback(this);
     }
-    
+
+    apaga() {
+
+        this._negociacoes = [];
+        this._callback(this);
+    }
+
     get negociacoes() {
-        
+
         return [].concat(this._negociacoes);
     }
 }
